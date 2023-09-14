e8224ee756fec8ea87a331d87d4631a9d87c7332
79166 <79166@LEXUS-666>
2023-09-14 17:31:27 +0300

    Добавьте файлы проекта.

---
+++ Lab 1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Person person = new Person("Иван", "Петров", 30);
+        person.DisplayInfo();
+        person.Age = 22;
+        person.DisplayInfo();
+        Console.ReadLine();
+    }
+}
+
+class Person
+{
+    private string firstName;
+    private string lastName;
+    private int age;
+    public Person(string firstName, string lastName, int age)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Имя: {firstName}");
+        Console.WriteLine($"Фамилия: {lastName}");
+        Console.WriteLine($"Возраст: {age}");
+        Console.WriteLine();
+    }
+}
